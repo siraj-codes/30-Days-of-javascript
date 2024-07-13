906d85e78c0ac30d692e9b77adbd9a88946e9170
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-13 07:41:40 +0500

    Activity2.js completed

---
+++ DAY-1/Variables_&_DataTypes/Activity2.js
@@ -0,0 +1,7 @@
+// Activity 2: Constant Declaration
+
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+
+const isRaining = true;
+console.log(isRaining);
