4d87de86194d81816a41da7617d9a6c83a2108dd
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-13 07:55:09 +0500

    // Activity5.js completed

--- DAY-1/Variables_&_DataTypes/Activity5.js
+++ DAY-1/Variables_&_DataTypes/Activity5.js
@@ -1,1 +1,14 @@
-// Activity4.js completed
+// Activity5.js completed
+
+// Activity 5: Understanding const
+
+
+// Task 6: Try reassigning a variable declared with const and observe the error.
+
+const UNCHANGABLE = 'YOU CAN NOT CHANGE ME !!!!!!';
+
+console.log(UNCHANGABLE);
+
+// TypeError: Assignment to constant variable.
+// at Object.<anonymous> (c:\Users\PMLS\Desktop\30 days of js\DAY-1\Variables_&_DataTypes\Activity5.js:12:13)
+UNCHANGABLE = 'I CHANGED YOU';
