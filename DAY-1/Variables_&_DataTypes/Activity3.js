72eddc43bad83be20671567d037dce2424a24c91
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-13 07:47:50 +0500

    Activity3.js completed

---
+++ DAY-1/Variables_&_DataTypes/Activity3.js
@@ -0,0 +1,22 @@
+// Activity2.js completed
+
+// Activity 3: Data Types
+
+
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+
+let n = 1;
+let name = 'sam';
+let isTall = false;
+let person = {
+    name : 'Harry',
+    age : 23,
+};
+let colors = ['red','green','blue'];
+
+console.log('Type of n is : ' ,typeof(n));
+console.log('Type of name is : ' ,typeof(name));
+console.log('Type of isTall is : ' ,typeof(isTall));
+console.log('Type of person is : ' ,typeof(person));
+console.log('Type of colors is : ' ,typeof(colors));
