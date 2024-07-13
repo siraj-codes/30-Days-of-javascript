dba42fa96cc6cd3e48fa40312ed526edec1b3054
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-13 07:38:25 +0500

    Activity1.js Completed

--- DAY-1/Variables_&_DataTypes/Activity1.js
+++ DAY-1/Variables_&_DataTypes/Activity1.js
@@ -1,4 +1,15 @@
 // Activity 1 : Variable Declaration
 
-// Task 1
+
+
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+var number = 12;
+console.log(number);
+
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+let name = "SirajAhmed";
+console.log(name);
+
 
