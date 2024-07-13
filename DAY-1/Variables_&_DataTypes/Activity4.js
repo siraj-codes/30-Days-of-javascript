ad61f4f63b999982b8b699786fddce4fa194863b
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-13 07:51:17 +0500

    // Activity4.js completed

--- DAY-1/Variables_&_DataTypes/Activity4.js
+++ DAY-1/Variables_&_DataTypes/Activity4.js
@@ -1,1 +1,15 @@
 // Activity4.js completed
+
+// Activity 4: Reassigning Variables
+
+
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log
+
+
+let Animal = 'Cow';
+
+console.log('Here Animal is ' , Animal);
+
+Animal = 'Cat';
+
+console.log('Here Animal is ' , Animal);
