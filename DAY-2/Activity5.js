2e584ceb44cdb01c07c148f717f7a8bb7ecedc13
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-14 17:19:13 +0500

    Activity5 completed

---
+++ DAY-2/Activity5.js
@@ -0,0 +1,10 @@
+// Activity 5: Ternary Operator
+
+
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let number = -121;
+
+let res = number>0 ? 'Number is Positive' : 'Number is Negative' 
+
+console.log(`\n${res}\n`)
