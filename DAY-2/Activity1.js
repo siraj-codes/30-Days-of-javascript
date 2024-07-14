1df04c1536582edab48ec3f0d01d7ee9b2a24d46
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-14 16:54:38 +0500

    Activity1-completed

---
+++ DAY-2/Activity1.js
@@ -0,0 +1,21 @@
+// Activity 1: Arithmetic Operations
+
+
+// Task 1: Write a program to add two numbers and log the result to the console. Task 2: Write a program to subtract two numbers and log the result to the console.
+
+let n1 = 12;
+let n2 = 45;
+
+let additon = n1+n2;
+
+console.log(`Additon is ${n1} + ${n2}  = ${additon}\n`)
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+let multiply = n1*n2;
+console.log(`Multiplication is ${n1} * ${n2}  = ${multiply}\n`)
+// Task 4: Write a program to divide two numbers and log the result to the console.
+let Div = n1/n2;
+console.log(`Divison is ${n1} / ${n2}  = ${Div}\n`)
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+let Rem = n1%n2;
+console.log(`Reminder is ${n1} % ${n2}  = ${Rem}\n`)
