7c5da88109cafea0cf5a653d591f6f65c9f915eb
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-14 16:58:20 +0500

    Activity2 completed

---
+++ DAY-2/Activity2.js
@@ -0,0 +1,10 @@
+// Activity 2: Assignment Operators
+
+
+// Task 6: Use the operator to add a number to a variable and log the result to the console.
+        let n1 = 12;
+        let n2 = n1;
+        console.log(`value of n1 is ${n1} and n2 is ${n2}\n`);
+// Task 7: Use the operator to subtract a number from a variable and log the result to the console.
+        n2 = n1-6;
+        console.log(`value of n1 is ${n1} and n2 is ${n2}\n`)
