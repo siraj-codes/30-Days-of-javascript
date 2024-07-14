c5215cb2fc39c55c3708fa1cbe2773d71cbd76a3
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-14 17:06:09 +0500

    Activity3 completed

---
+++ DAY-2/Activity3.js
@@ -0,0 +1,12 @@
+// Activity 3: Comparison Operators
+
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let n1 = 55;
+let n2 = 45;
+let n3 = 45;
+let n4 = '45';
+console.log(`\n${n1} is greater then ${n2} ${n1>n2} , ${n1} is lesser then ${n2} ${n1<n2}\n` );
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+console.log(`\n${n2} is greater or equal then ${n3} ${n2>=n3} , ${n2} is lesser or equal then ${n3} ${n2<=n3}\n` );
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+console.log(`\n${n2} is equal (==) to ${n4} ${n2==n4} , ${n2} is  equal(===) to ${n4} ${n2===n4}\n` );
