a8d1672705453bbd1eddfbda3fc1e703fa11c41e
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-15 17:31:25 +0500

    Activity4 completed

---
+++ DAY-3/Control-Structures/Activity4.js
@@ -0,0 +1,15 @@
+// Activity 4: Conditional (Ternary) Operator
+
+
+
+// Task 6: Write a program that uses the ternary operator to check if a number is even or odd and log the result to the console.
+
+
+let number = 12;
+
+let isEven = number%2==0 ? 'Even' : 'Odd';
+
+console.log(`\nNumber ${number} is ${isEven}\n`)
+
+
+
