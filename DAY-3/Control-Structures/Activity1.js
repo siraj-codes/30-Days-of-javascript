36d82c536f54719b9610d7cf62e92ff6276fdc37
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-15 17:08:53 +0500

    Activity-1 Completed

---
+++ DAY-3/Control-Structures/Activity1.js
@@ -0,0 +1,32 @@
+// Activity 1: If-Else Statements
+
+
+// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console.
+let n1 = 0;
+
+if (n1 > 0) {
+    console.log('\nNumber is Positive\n');
+}
+
+else if (n1 < 0) {
+    console.log('\nNumber is Negative\n');
+
+}
+else {
+    console.log('\nNumber is Zero\n');
+}
+
+// Task 2: Write a program to check if a person is eligible to vote (age >18) and log the result to the console.
+
+let age = 19;
+
+if (age > 18) {
+
+    console.log('\nYou are eligible for vote\n');
+    
+}
+
+else {
+    
+    console.log('\nYou are not-eligible for vote\n');
+}
