e8ad1854173f8e638ba283ee902ecdf0a6b43883
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-16 19:18:18 +0500

    Activity2 completed

---
+++ DAY-4/LOOPS/Activity2.js
@@ -0,0 +1,22 @@
+// Activity 2: While Loop
+
+// Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let i = 1;
+let sum = 0;
+while (i<11) {
+
+    sum+=i;
+    i++;
+}
+
+console.log('Sum is :' ,sum);
+// Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+
+ i = 10
+
+ while (i>0) {
+
+    console.log(i)
+    i--;
+    
+ }
