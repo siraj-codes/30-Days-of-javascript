a2b8863b6104ff5f0badffe0bbed7d34ebe3ece2
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-16 19:14:41 +0500

    Activity1 completed

---
+++ DAY-4/LOOPS/Activity1.js
@@ -0,0 +1,14 @@
+// Activity 1: For Loop
+
+// Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+for (let index = 1; index < 11; index++) {
+    console.log(`\t${index}`);
+    
+}
+console.log('\n');
+// Task 2: Write a program to print the multiplication table of 5 using a for loop.
+for (let index = 1; index < 11; index++) {
+    console.log(`\t 5 x ${index} = ${5*index}`);
+    
+}
+console.log('\n');
