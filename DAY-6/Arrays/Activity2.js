022c63746e23eb1716dcb663edd386b993038615
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-18 12:17:48 +0500

    Activity2 completed

---
+++ DAY-6/Arrays/Activity2.js
@@ -0,0 +1,23 @@
+// Activity 2: Array Methods (Basic)
+
+// Task 3: Use the push method to add a new number to the end of the array and log the updated array.
+let numbers = [2, 4, 6, 8];
+console.log(numbers);
+numbers.push(10);
+console.log(numbers);
+
+
+
+// Task 4: Use the pop method to remove the last element from the array and log the updated array.
+let popped = numbers.pop();
+console.log(`popped val is ${popped}`);
+console.log(numbers);
+
+
+// Task 5: Use the shift method to remove the first element from the array and log the updated array.
+let shifted = numbers.shift();
+console.log(`Shifted numbers is ${shifted}`);
+console.log(numbers);
+// Task 6: Use the unshift method to add a new number to the beginning of the array and log the updated array.
+
+console.log(numbers);
