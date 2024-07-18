6cdac9345b33d5e82eeecbbd9268f6cf3038f9f5
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-18 12:12:21 +0500

    Activity1 completed

---
+++ DAY-6/Arrays/Activity1.js
@@ -0,0 +1,8 @@
+// Activity 1: Array Creation and Access
+
+// Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+// Task 2: Access the first and last elements of the array and log them to the console.
+console.log(numbers[0]);
+console.log(numbers[4]);
