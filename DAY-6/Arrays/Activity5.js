97f5a0ffd0b3440f2d9369b8ab0934587e58355e
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-18 12:34:42 +0500

    Activity5 completed

---
+++ DAY-6/Arrays/Activity5.js
@@ -0,0 +1,14 @@
+// Activity 5: Multi-dimensional Arrays
+
+// Task 12: Create a two-dimensional array (matrix) and log the entire array to the console.
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+console.log(matrix);
+// Task 13: Access and log a specific element from the two-dimensional array.
+
+console.log(matrix[1][2]);
