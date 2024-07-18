892e93e6fb277d88ce7a601da1e3aca5b2a60d10
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-18 11:52:20 +0500

    Activity4 completed

---
+++ DAY-5/Functions/Activity4.js
@@ -0,0 +1,12 @@
+// Activity 4: Function Parameters and Default Values
+
+// Task 7: Write a function that takes two parameters and returns their product. Provide a default value for the second parameter. • 
+let mul = (n1, n2 = 1) => `${n1} x ${n2} = ${n1 * n2}`;
+console.log(mul(2, 5));
+
+// Task 8: Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
+let greeting = (name, age = 10) => `Hello Dear ${name} :) 
+we are very pleased to see you 
+we hope you are doing great!`;
+console.log(greeting('Siraj'))
+
