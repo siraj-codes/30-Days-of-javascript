2e8406f76227b32515fe7df26b0cd886c1536720
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-24 08:38:29 +0500

    Activity1 Completed

---
+++ DAY-8/ES6-Features/Activity1.js
@@ -0,0 +1,12 @@
+// Activity 1: Template Literals
+
+// Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+let personName = 'Seth Coleman';
+let personAge = '35';
+console.log(`Person's Name is ${personName} and Person's Age is ${personAge}`);
+// Task 2: Create a multi-line string using template literals and log it to the console.
+console.log(`epzid@odube.sc
+                            Colombia
+        rgb(63,123,253)
+            2480:d57c:4c4c:a908:8296:518b:5ca8:9bf4
+    `)
