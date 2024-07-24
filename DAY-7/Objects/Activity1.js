8109ed061e3d4eeb21b5b1e58700e10500831c00
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-24 07:53:23 +0500

    Activity1 completed

---
+++ DAY-7/Objects/Activity1.js
@@ -0,0 +1,17 @@
+// Activity 1: Object Creation and Access
+
+
+// Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console.
+let book = {
+
+    title: 'Diode&Properties',
+    author: 'AsifKhan',
+    year: '2022',
+}
+
+console.log(book);
+
+
+// Task 2: Access and log the title and author properties of the book object.
+
+console.log(book['title'], book['author'])
