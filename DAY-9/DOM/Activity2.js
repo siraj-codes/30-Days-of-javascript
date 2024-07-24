e024c10925610d8a133221c90f49e68a9d8e3aab
siraj-codes <sirajahmed.bscsf23@iba-suk.edu.pk>
2024-07-24 20:20:40 +0500

    Activity2 Completed

---
+++ DAY-9/DOM/Activity2.js
@@ -0,0 +1,9 @@
+// Activity 2: Creating and Appending Elements
+
+
+
+// • Task 3: Create a new div element with some text content and append it to the body.
+
+let div = document.createElement("div")
+div.textContent = "Siraj is a good boy....."
+document.body.appendChild(div)
